b058682ab7279bb83ba684b586c486a7921ed981
Patrick Kay <pckay96@gmail.com>
2023-09-24 16:47:58 -0500

    Added player with space firing

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    public float speed;
+    private float Move;
+    private Color newCol;
+    private Color oldCol;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+        
+        oldCol = spr.material.GetColor("_Color");
+        newCol = new Color(0.4f, 0.9f, 0.7f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+
+        if (Input.GetKey("space") == true)
+        {
+            spr.material.SetColor("_Color", newCol);
+            Debug.Log("PEW");
+        }
+        if (Input.GetKey("space") == false)
+        {
+            spr.material.SetColor("_Color", oldCol);
+        }
+    }
+}
