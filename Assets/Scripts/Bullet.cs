1791ace7c813403b2141374b5440eba29c2eb3e4
Patrick Kay <pckay96@gmail.com>
2023-09-27 18:02:54 -0500

    Tried to slow down rate of fire

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,8 +6,13 @@ public class Bullet : MonoBehaviour
 {
     public Vector3 direction;
     public float speed;
+    private float nextShootTime = 0.0f;
+    private float period = 0.1f;
     
     private void Update(){
-        this.transform.position += this.direction * this.speed * Time.deltaTime;
+        if(Time.time > nextShootTime){
+            nextShootTime += period;
+            this.transform.position += this.direction * this.speed * Time.deltaTime;
+        }
     }
 }
