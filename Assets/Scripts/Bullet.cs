6f743ca0bbe8aed652f95eb5ab0c3eeaa52b60b8
Laon <reeedt@gmail.com>
2023-10-01 21:21:50 -0400

    Finished basic bullet and enemy

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,17 +6,35 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 1f;
     private Rigidbody2D rb2D;
-    private Vector2 velocity;
-
+    private Vector2 velocity = Vector2.up;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        velocity = speed * new Vector2(0f, 1f);
     }
 
     void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void send_off(Vector2 direction, float speed_multiplier)
+    {
+
+
+        velocity = speed * direction * speed_multiplier;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        } else if (other.gameObject.CompareTag("Player"))
+        {
+            // reduce player life
+        }
+
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Rigidbody2D bullet;
+    public Bullet bullet;
+    public float bullet_speed = 1f;
+    float timeLeft = 2.0f;
+    Vector3 firing_point_offset = new Vector3(0, -0.25f, 0);
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +21,14 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = 2.0f;
+            Bullet new_bullet = Instantiate(bullet, transform.position + firing_point_offset, Quaternion.identity);
+            new_bullet.send_off(Vector2.down, bullet_speed);
+        }
     }
+
+    
 }
