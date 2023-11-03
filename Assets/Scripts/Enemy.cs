e75ddbf2d16dde73dbd293f4e8b590e7fc254836
beatoven9 <reeedt@gmail.com>
2023-11-03 16:50:59 -0600

    Enemy can_shoot is now determined by a raycast.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Enemy : MonoBehaviour
 {
     public bool can_shoot = false;
@@ -15,7 +14,7 @@ public class Enemy : MonoBehaviour
     public BoxCollider2D collider;
     public Rigidbody2D powerUpPrefab;
     public float chance = 5f;
-    
+
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -31,6 +30,21 @@ public class Enemy : MonoBehaviour
             timeLeft = 2.0f;
             fire();
         }
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down);
+		if (hit.collider != null) {
+			if (hit.collider.CompareTag("Enemy")){
+				Debug.Log("This is an enemy, I can't shoot");
+				can_shoot = false;	
+			} else {
+				Debug.Log("!!!" + hit.collider.tag);
+				can_shoot = true;
+			}
+		} else if (hit.collider == null) {
+			Debug.Log("Didnt get a hit");
+			can_shoot = true;
+		}
+			
     }
 
     void fire(){
@@ -52,11 +66,11 @@ public class Enemy : MonoBehaviour
                 Rigidbody2D powerUpPrefabClone;
                 powerUpPrefabClone = Instantiate(powerUpPrefab, transform.position, transform.rotation) as Rigidbody2D;
             }
-            Death();
+            Die();
         }
     }
     
-    void Death()
+    void Die()
     {
         collider.enabled = false;
         anim.SetTrigger("Death");
