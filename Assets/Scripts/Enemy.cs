1a44530360e5e1fa683649395f0cff52200885dd
Patrick Kay <pckay96@gmail.com>
2023-11-03 11:23:49 -0500

    Powerup drop set up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -99,8 +99,18 @@ public class Enemy : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
-            
-            Die();
+            float randomValue = Random.value;
+            if (randomValue < chance) {
+                Rigidbody2D powerUpPrefabClone;
+                powerUpPrefabClone = Instantiate(powerUpPrefab, transform.position, transform.rotation) as Rigidbody2D;
+            } else if (randomValue < chance2) {
+                Rigidbody2D powerUpPrefabClone;
+                powerUpPrefabClone = Instantiate(powerUpPrefab2, transform.position, transform.rotation) as Rigidbody2D;
+            } else if (randomValue < chance3) {
+                Rigidbody2D powerUpPrefabClone;
+                powerUpPrefabClone = Instantiate(powerUpPrefab3, transform.position, transform.rotation) as Rigidbody2D;
+            }
+            Death();
         }
     }
     
