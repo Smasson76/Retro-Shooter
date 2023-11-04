1190a7ce5b78e635255cdf34155bc80ea6615b8a
Patrick Kay <pckay96@gmail.com>
2023-11-04 13:50:06 -0500

    PowerUp bug fix

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,16 +12,7 @@ public class Enemy : MonoBehaviour
     public GameObject PointOfFireObject;
     public Animator anim;
     public BoxCollider2D collider;
-    public Rigidbody2D powerUpPrefab;
-    public Rigidbody2D powerUpPrefab2;
-    public Rigidbody2D powerUpPrefab3;
-    public Rigidbody2D powerUpPrefab4;
-    public float chance = 1.5f;
-    public float chance2 = 3f;
-    public float chance3 = 5f;
-    public float chance4 = 7f;
-
-	public Vector2 firing_window = new Vector2(1.5f, 2.5f);
+    public Vector2 firing_window = new Vector2(1.5f, 2.5f);
 
     public AudioClip enemy_death;
     Vector2 origin_position;
@@ -32,16 +23,6 @@ public class Enemy : MonoBehaviour
 		float next_firing_time = Random.Range(firing_window.x, firing_window.y);
 		return next_firing_time;
 	}
-
-    public Rigidbody2D powerUpPrefab2;
-    public Rigidbody2D powerUpPrefab3;
-    public Rigidbody2D powerUpPrefab4;
-    public float chance = 1.5f;
-    public float chance2 = 3f;
-    public float chance3 = 5f;
-    public float chance4 = 7f;
-
-    
     void Awake()
     {
         anim = GetComponent<Animator>();
@@ -93,17 +74,7 @@ public class Enemy : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
-            float randomValue = Random.value;
-            if (randomValue < chance) {
-                Rigidbody2D powerUpPrefabClone;
-                powerUpPrefabClone = Instantiate(powerUpPrefab, transform.position, transform.rotation) as Rigidbody2D;
-            } else if (randomValue < chance2) {
-                Rigidbody2D powerUpPrefabClone;
-                powerUpPrefabClone = Instantiate(powerUpPrefab2, transform.position, transform.rotation) as Rigidbody2D;
-            } else if (randomValue < chance3) {
-                Rigidbody2D powerUpPrefabClone;
-                powerUpPrefabClone = Instantiate(powerUpPrefab3, transform.position, transform.rotation) as Rigidbody2D;
-            }
+            
             Die();
         }
     }
@@ -112,6 +83,7 @@ public class Enemy : MonoBehaviour
     {
         collider.enabled = false;
         anim.SetTrigger("Death");
+        GameManager.instance.enemyCount -= 1;
         musicManager.Instance.playSound("enemy_die");
         Destroy(this.gameObject, 0.8f); 
     }
