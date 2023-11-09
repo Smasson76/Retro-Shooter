0cf0b42f97eddbdc3055ce9ea7fa11ac9d241fd4
Laon <reeedt@gmail.com>
2023-11-09 08:45:49 -0700

    Reformatted musicmanager code

--- Assets/Scripts/MusicManager/musicManager.cs
+++ Assets/Scripts/MusicManager/musicManager.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class musicManager : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class musicManager : MonoBehaviour{
     public static musicManager Instance;
 
     public Sound[] musicSounds , sfxSounds;
     [SerializeField] public AudioSource _musicSrc,_FXsrc;
-   void Awake(){
+
+    void Awake(){
         if(Instance == null){
             Instance = this;
             DontDestroyOnLoad(gameObject);
         } else{
             Destroy(gameObject);
         }
-   }
-   private void Start(){
-    playMusic("StartMenu");
-   }
-   public void playMusic(string name){
-   	 Sound s = Array.Find(musicSounds , x => x.name == name);
-   	 if(s==null){
-   	     Debug.Log("Sound not found!");
-   	 }
-   	 else
-   	 {
-   	     _musicSrc.clip = s.clip;
-   	     _musicSrc.Play();
-   	 }
-	}
+    }
+
+    private void Start(){
+        playMusic("StartMenu");
+    }
+
+    public void playMusic(string name){
+        Sound s = Array.Find(musicSounds , x => x.name == name);
+   	    if(s==null){
+   	        Debug.Log("Sound not found!");
+   	    } else {
+   	        _musicSrc.clip = s.clip;
+   	        _musicSrc.Play();
+   	    }
+    }
 
 	public string getCurrentTrack(){
 		string current_track_name = _musicSrc.clip.name;
@@ -44,10 +43,9 @@ public class musicManager : MonoBehaviour
 		}	
 		Debug.Log("No hit found for " + current_track_name);
 	    return _musicSrc.name;
-   }
+    }
 
-   public void playSound(string name)
-   {
+    public void playSound(string name){
         Sound s = Array.Find(sfxSounds,x=>x.name == name);
         if(s==null){
             Debug.Log("Sound not found!");
@@ -56,15 +54,16 @@ public class musicManager : MonoBehaviour
             _FXsrc.clip = s.clip;
             _FXsrc.Play();
         }
-   }
+    }
 
-    public void ChangeMasterVolume(float value)
-    {
+    public void ChangeMasterVolume(float value){
         AudioListener.volume = value;
     }
+
     public void ToggleMusic(){
         _musicSrc.mute = ! _musicSrc.mute;
     }
+
     public void ToggleFX(){
         _FXsrc.mute = ! _FXsrc.mute;
     }
